827f1b9998012d370cde83d3905ced33fde5ecd7
Griffin <griffincriste@gmail.com>
2020-06-02 13:58:09 -0500

    working on deployment

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '..', 'config', 'config.js'))[env];
+const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 const db = {};
