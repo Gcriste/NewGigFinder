d81897e3a3bad7a168acf39e9ed26e8048872da7
Griffin <griffincriste@gmail.com>
2020-06-02 12:48:07 -0500

    second try

--- config/config.js
+++ config/config.js
@@ -40,12 +40,16 @@ module.exports = {
 		dialect: 'mysql',
 		logging: false
 	},
+	// production: {
+	// 	username: 'd95e37m3vi7ybc9w',
+	// 	password: 'wswp8s7kdoltl3af',
+	// 	database: 'x1gjaz2swejml6hh',
+	// 	host: 'thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	// 	dialect: 'mysql',
+	// 	logging: false
+	// }
 	production: {
-		username: 'd95e37m3vi7ybc9w',
-		password: 'wswp8s7kdoltl3af',
-		database: 'x1gjaz2swejml6hh',
-		host: 'thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		dialect: 'mysql',
-		logging: false
+		use_env_variable: 'JAWSDB_URL',
+		dialect: 'mysql'
 	}
 };
